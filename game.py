7c1c2fe83e23783e5f9e088edff1631283076f5e
Derias <96659633+Derias20@users.noreply.github.com>
2022-07-04 23:06:09 +0200

    Update game.py

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 #Game made by Derias => https://github.com/Derias20
-#Game is bugging after three rounds, Im gonna make a fix it later.
+#Game is bugging after three rounds, Im going to make a fixing update later.
 import random
 while True:
     possible_choices = ["rock" , "paper" , "scissors"]
